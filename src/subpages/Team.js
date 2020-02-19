f8d99abc7213505996e99c38f45e899da78457cb
MaxRobertsDear <maxhroberts@gmail.com>
2020-02-19 22:12:37 +0000

    Project Setup

---
+++ src/subpages/Team.js
@@ -0,0 +1,38 @@
+import React from "react"
+import TeamItem from "../components/TeamItem"
+import { useStaticQuery, graphql } from "gatsby"
+
+function Team() {
+  const { allTeamYaml } = useStaticQuery(graphql`
+    {
+      allTeamYaml {
+        edges {
+          node {
+            name
+            img
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <div
+      id="team"
+      className="container-fluid"
+      style={{ backgroundColor: "#AFECA4" }}
+    >
+      <div className="container">
+        <div className="flexbox">
+          <h1 className="text-center my-4">Who we work with </h1>
+          <div className="row my-5">
+            {allTeamYaml.edges.map(({ node }) => {
+              return <TeamItem title={node.name} src={node.img} />
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Team
